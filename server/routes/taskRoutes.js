ee2ddf57b93581c11b27ab3e660ab97eb2c84d3b
Mohd Huzaifa <112268278+m-3huzaifa1@users.noreply.github.com>
2025-05-16 12:22:10 +0530

    Update taskRoutes.js

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -21,7 +21,7 @@ router.post('/create', auditTaskCreateActions, createTask)
 
 router.put('/:id', auditTaskActions, updateTask)
 
-router.delete('/:id', auditTaskActions, deleteTask);
+router.post('/:id/delete', auditTaskActions, deleteTask);
 
 router.put('/:id/assign', auditAssignment, assignTask);
 
